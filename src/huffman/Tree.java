964e7c531ca842d5d7dda7e77376cc9c14257d4b
unknown <joakimdeak@hotmail.com>
2019-05-11 00:34:20 +0200

    added tree class

---
+++ src/huffman/Tree.java
@@ -0,0 +1,36 @@
+package huffman;
+
+import java.util.ArrayList;
+
+public class Tree {
+	private Node root;
+
+	public Tree(Node root) {
+		this.root = root;
+	}
+
+	public void print() {
+		this.root.print();
+	}
+
+	public int size() {
+
+		ArrayList<Node> list = new ArrayList<Node>();
+
+		list.add(this.root);
+
+		int pointer = 0;
+		while (pointer < list.size()) {
+			Node current = list.get(pointer);
+			if (current.right() != null) {
+				list.add(current.right());
+			}
+			if (current.left() != null) {
+				list.add(current.left());
+			}
+			pointer++;
+		}
+
+		return list.size();
+	}
+}
