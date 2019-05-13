567767af698c4aa924b13f2f3af025ee559a0a24
JoakimDeak <44032267+JoakimDeak@users.noreply.github.com>
2019-05-13 10:07:20 +0200

    Added method that writes encoded output in binary

--- Encoder.java
+++ Encoder.java
@@ -3,9 +3,11 @@ package huffman;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.Scanner;
 
 public class Encoder {
@@ -36,7 +38,8 @@ public class Encoder {
 	private void encode(ArrayList<String> charCodes) throws IOException {
 
 		Scanner sc = new Scanner(this.inputFile);
-		String outputFileName = "Encoded-" + this.inputFile.getName().substring(0, this.inputFile.getName().lastIndexOf('.')) + ".bin";
+		String outputFileName = "Encoded-"
+				+ this.inputFile.getName().substring(0, this.inputFile.getName().lastIndexOf('.')) + ".bin";
 		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(outputFileName)));
 
 		while (sc.hasNextLine()) { // goes through every line
@@ -54,5 +57,51 @@ public class Encoder {
 
 		sc.close();
 		writer.close();
+	}
+// writes the encoded text as binary to actually compress original text
+	private void testEncode(ArrayList<String> charCodes) throws IOException {
+		Scanner sc = new Scanner(this.inputFile);
+		FileOutputStream fos = new FileOutputStream(new File("output.bin"));
+
+		StringBuilder encoded = new StringBuilder();
+		int encodedLengthBeforeWrite = 256; // how many bits will be written at a time
+
+		while (sc.hasNextLine()) { // go through every line
+			String cLine = sc.nextLine();
+			for (int i = 0; i < cLine.length(); i++) { // go through every character
+				char cChar = cLine.charAt(i);
+				for (String code : charCodes) { // find entry for current character in
+					if (code.charAt(0) == cChar) {
+						encoded.append(code.substring(1)); // add character code to stringbuilder
+					}
+				}
+				if (encoded.length() >= encodedLengthBeforeWrite) {
+					encoded = write(encodedLengthBeforeWrite, encoded, fos);
+				}
+			}
+		}
+		if(encoded.length() > 0) { // if number of characters wasnt multiple of the given number
+			int bitsToBeAdded = 8 - encoded.length() % 8; // make it multiple of eight by adding extra 0s
+			for(int i = 0; i < bitsToBeAdded; i++) {
+				encoded.append('0');
+			}
+			write(encoded.length(), encoded, fos); // write the remaining characters
+		}
+		sc.close();
+		fos.close();
+	}
+	
+	private StringBuilder write(int charsToWrite, StringBuilder input, FileOutputStream fos) throws IOException {
+		
+		BitSet set = new BitSet();
+		for (int j = 0; j < charsToWrite; j++) { // takes string of 0s and 1s and converts to bitset
+			if (input.charAt(j) == '1') {
+				set.set(j);
+			}
+		}
+		fos.write(set.toByteArray()); // writes the bitset to output file as byte array
+		input.delete(0, charsToWrite); // deletes characters once they have been written
+		
+		return input;
 	}
 }
